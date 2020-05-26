7bcef1c7fc4dfb8baf9d17c3371049e2a46fe1c7
ft-coder <juravlev.johm@gmail.com>
2020-05-26 12:49:20 +0500

    1

--- lab2-task5-mk-final/controller.py
+++ lab2-task5-mk-final/controller.py
@@ -16,17 +16,39 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.clearButt.clicked.connect(self.clear_plot)
         self.ui.graphicsView.setBackground(background='w')
         self.ui.graphicsView.showGrid(x=True, y=True)
+        self.ui.graphicsView.addLegend()
 
     @staticmethod
-    def create_random(l):
-        return -(math.log(random.random()) / l)
+    def create_random():
+        a = -0.01
+        b = 0.01
+        return a + (b - a) * random.random()
 
-    @staticmethod  # ((l ** k) * (x ** (k - 1)) * math.exp(-l * x)) / math.factorial(k - 1)
-    def function(x, k, l):
-        return (l ** k) * (x ** (k - 1)) * math.exp(-l * x) / math.factorial(k - 1)
+    def gen_plots(self, n, ro, m):
+        x_arr = numpy.arange(0, ro, 0.1)
+        y_arr = []
+        y_exp = []
+        sigmas = []
+        for x in x_arr:
+            if x < 1:
+                y_th = x / (1 - x ** (m + 2)) * (1 + x ** (m + 1) * (m * x - m + x - 2)) / (1 - x)
+            elif x == 1:
+                y_th = ((m + 1) / 2) * x
+            else:
+                y_th = x / (1 - x ** (m + 2)) * (1 + x ** (m + 1) * (m * x - m + x - 2)) / (1 - x)
+            y_arr.append(y_th)
 
-    def gen_plots(self, n, k, t, l):
-        pass
+        for y in y_arr:
+            rand = self.create_random()
+            val = y + rand
+            y_exp.append(val)
+
+        return {
+            "x": x_arr,
+            "y": y_arr,
+            "y_exp": y_exp,
+            "sigmas": sigmas
+        }
 
     def create_plot(self):
         n = float(self.ui.nEdit.text())
@@ -34,12 +56,25 @@ class MainWindow(QtWidgets.QMainWindow):
         ro = float(self.ui.roEdit.text())
         r = float(self.ui.REdit.text())
 
-        if self.ui.radio1.isChecked():
+        '''
+        n = 5000
+        m = 6
+        r = 15
+        ro = 3'''
+        
+        points = self.gen_plots(n, ro, m)
+        pen_style = pyqtgraph.mkPen('g', width=2)
+        self.ui.graphicsView.plot(points["x"], points["y"], pen=pen_style, name="Теория")
+        self.ui.graphicsView.plot(points["x"], points["y_exp"], name='Эксперимент', pen=None)
+        self.ui.graphicsView.plot(points["x"], points["sigmas"], name='Стандарт.откл', pen=None)        
+
+
+        '''if self.ui.radio1.isChecked():
             print(1)
         elif self.ui.radio2.isChecked():
             print(2)
         elif self.ui.radio3.isChecked():
-            print(3)
+            print(3)'''
 
     def clear_plot(self):
         self.ui.graphicsView.clear()
